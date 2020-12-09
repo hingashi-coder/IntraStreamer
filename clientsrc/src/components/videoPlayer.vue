d86fc2974d7a106ea76155727ee806cb59865b5a
k018c1069 <k018c1069@it-neec.jp>
2020-12-09 14:51:09 +0900

    エラーメッセージ仮実装

--- clientsrc/src/components/videoPlayer.vue
+++ clientsrc/src/components/videoPlayer.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
       <video ref="videoPlayer" class="video-js"></video>
+      <p>{{msg}}</p>
     </div>
 </template>
 
@@ -25,6 +26,17 @@ export default {
   mounted () {
     this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady () {
       console.log('onPlayerReady', this)
+    })
+    this.player.on('error', () => {
+      console.log(this.player.error().code)
+      switch (this.player.error().code) {
+        case 2:
+          this.msg = '配信を開始してください'
+          console.log(this.msg)
+          break
+        default:
+          this.msg = '不明なエラーです。'
+      }
     })
   },
   beforeDestroy () {
